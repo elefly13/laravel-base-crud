180aed99efffc69fe3e733080e19adf666ab1acf
Elefly13 <elymara13@gmail.com>
2021-11-03 00:47:06 +0100

    work in progress

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,5 +3,14 @@
 @section('content')
     <div class="container">
         
+        <ul>
+            @foreach ($comics as $comic)
+            <a href=" {{ route('comics.show')}}">
+                <li>{{$comic['title']}}</li>
+            </a>
+            @endforeach
+            
+        </ul>
+
     </div>
 @endsection
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,5 +1,7 @@
 @extends('layout.app')
 
 @section('content')
-    
+    <div class="container">
+        <h1>dettaglio fumetto</h1>
+    </div>
 @endsection
